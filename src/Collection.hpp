76cb5ed634d74f76838059ddf9a7d2ba8de65aa8
Patryk Kaźmierczak <github.scarcity013@passmail.net>
2025-04-12 18:25:37 +0200

    Add virtual methods to Collection class for managing elements

--- src/Collection.hpp
+++ src/Collection.hpp
@@ -1,10 +1,15 @@
 #ifndef COLLECTION_HPP
 #define COLLECTION_HPP
-
-
+#include "Node.hpp"
 
 class Collection {
-
+public:
+  virtual ~Collection() = default;
+  virtual void insert(int value, int priority) = 0;
+  virtual Node extractMax() = 0;
+  virtual Node peek() = 0;
+  virtual void modifyKey(int value, int newPriority) = 0;
+  virtual void getSize() = 0;
 };
 
 
