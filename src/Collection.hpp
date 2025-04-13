af78def1f81e94e1c9994cb892e1f4b47864a28c
Patryk Kaźmierczak <github.scarcity013@passmail.net>
2025-04-13 14:59:32 +0200

    Refactor Collection class to add getLevels method and change getHeight return type

--- src/Collection.hpp
+++ src/Collection.hpp
@@ -1,15 +1,18 @@
 #ifndef COLLECTION_HPP
 #define COLLECTION_HPP
+#include <vector>
 
 class Collection {
 public:
   virtual ~Collection() = default;
+  virtual std::vector<std::vector<int>> getLevels() = 0;
   virtual void insert(int value, int priority) = 0;
   virtual int extractMax() = 0;
   virtual int peek() = 0;
   virtual void modifyKey(int value, int newPriority) = 0;
-  virtual void getHeight() = 0;
+  virtual int getHeight() = 0;
   [[nodiscard]] int getSize() const { return size; }
+
 protected:
   void setSize(const int newSize) { size = newSize; }
 
