91659fa383028412e554e8ecf1049352ccb914de
Patryk Kaźmierczak <github.scarcity013@passmail.net>
2025-04-13 11:48:57 +0200

    Refactor Collection class to change return types of extractMax and peek methods, and add size management methods

--- src/Collection.hpp
+++ src/Collection.hpp
@@ -1,17 +1,21 @@
 #ifndef COLLECTION_HPP
 #define COLLECTION_HPP
-#include "Node.hpp"
 
 class Collection {
 public:
   virtual ~Collection() = default;
   virtual void insert(int value, int priority) = 0;
-  virtual Node extractMax() = 0;
-  virtual Node peek() = 0;
+  virtual int extractMax() = 0;
+  virtual int peek() = 0;
   virtual void modifyKey(int value, int newPriority) = 0;
   virtual void getSize() = 0;
+
+protected:
+  void increaseSize() { size++; }
+  void decreaseSize() { size--; }
+
+private:
+  int size = 0;
 };
 
-
-
-#endif //COLLECTION_HPP
+#endif // COLLECTION_HPP
