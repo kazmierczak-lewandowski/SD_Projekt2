a66779ef0114b3366682e2bf3dd02427d09a0427
Patryk Kaźmierczak <github.scarcity013@passmail.net>
2025-04-14 14:37:51 +0200

    Implement assignment operator and enhance equality check in Element class

--- src/Element.cpp
+++ src/Element.cpp
@@ -1,1 +1,8 @@
 #include "Element.hpp"
+Element &Element::operator=(const Element &other) {
+  if (this != &other) {
+    value = other.value;
+    priority = other.priority;
+  }
+  return *this;
+}
--- src/Element.hpp
+++ src/Element.hpp
@@ -6,12 +6,14 @@ class Element {
 public:
   Element(const int value, const int priority)
       : value(value), priority(priority) {}
+  Element() : value(-1), priority(-1) {}
   auto operator<=>(const Element &other) const {
     return priority <=> other.priority;
   };
   bool operator==(const Element &other) const {
-    return priority == other.priority;
+    return (priority == other.priority) && (value == other.value);
   }
+  Element &operator=(const Element &other);
   [[nodiscard]] int getValue() const { return value; }
   [[nodiscard]] int getPriority() const { return priority; }
   [[nodiscard]] bool checkValue(const Element &other) const {
@@ -21,7 +23,7 @@ public:
     return value == otherValue;
   }
 private:
-  const int value;
+  int value;
   int priority;
 };
 
