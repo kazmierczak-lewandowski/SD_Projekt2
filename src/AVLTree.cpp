8efda0c2d8fd82a86c47efc3dffebfe78fa0b831
piotr <piotr.lewandowski09@gmail.com>
2025-04-16 12:35:28 +0200

    Implement AVLTree class: add insert, modifyKey, peek, extractMax, and findElement methods; define AVLNode structure

--- src/AVLTree.cpp
+++ src/AVLTree.cpp
@@ -1,1 +1,34 @@
 #include "AVLTree.hpp"
+void AVLTree::insert(Element element) {
+  auto newNode = std::make_unique<AVLNode>(element);
+  if (getSize() == 0) {
+    root = std::move(newNode);
+    return;
+  }
+  auto current = root.get();
+  for (int i = getHeight(); i>=0; i++) { //for or while?
+    if (newNode->element < current->element) {
+      current = current->left.get();
+    } else {
+      current = current->right.get();
+    }
+  }
+  newNode.reset(current);
+}
+void AVLTree::modifyKey(const Element& element, int newPriority){
+
+}
+Element AVLTree::peek() const {
+
+}
+Element AVLTree::extractMax() {
+
+}
+std::vector<std::vector<Element>> AVLTree::getLevels() const {
+
+}
+int AVLTree::findElement(const Element& element, int index) const {
+
+}
+
+
--- src/AVLTree.hpp
+++ src/AVLTree.hpp
@@ -1,6 +1,29 @@
 #ifndef AVLTree_HPP
 #define AVLTree_HPP
+#include <memory>
 
-class AVLTree {};
+#include "Collection.hpp"
+
+class AVLTree final : public Collection{
+private:
+  struct AVLNode{
+    Element element;
+    std::unique_ptr<AVLNode> left = nullptr;
+    std::unique_ptr<AVLNode> right = nullptr;
+    AVLNode *parent = nullptr;
+    int height = 0;
+    explicit AVLNode(Element const e) : element(e){}
+  };
+  std::unique_ptr<AVLNode> root = nullptr;
+public:
+  explicit AVLTree() = default;
+  ~AVLTree() override = default;
+  [[nodiscard]] std::vector<std::vector<Element>> getLevels() const override;
+  void insert(Element element) override;
+  Element extractMax() override;
+  [[nodiscard]] Element peek() const override;
+  [[nodiscard]] int findElement(const Element& element, int index) const override;
+  void modifyKey(const Element& element, int newPriority) override;
+};
 
 #endif // AVLTree_HPP
