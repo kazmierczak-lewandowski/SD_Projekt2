f16d1161f44c3d6108ef0a734c51da9cd68e5488
Patryk Kazmierczak <github.scarcity013@passsmail.net>
2025-04-17 17:08:39 +0200

    Refactor Collection class: improve formatting logic in print method and enhance display of elements

--- src/Collection.hpp
+++ src/Collection.hpp
@@ -27,19 +27,45 @@ public:
       for (int j = 0; j < levels[i].size(); j++) {
         auto element = levels[i][j];
         auto elementString = element.toString();
-        int numOfSpaces = 0;
+        int numOfSpaces = 1;
         if (!prevSpaces.empty()) {
-          numOfSpaces =
-              static_cast<int>(prevSpaces[j] - elementString.find(';') - levelString.size());
-          levelString += std::string(numOfSpaces, ' ');
+
+          numOfSpaces = static_cast<int>(prevSpaces[j] - levelString.size());
+          levelString += std::string(static_cast<int>(prevSpaces[j] -
+                                                      elementString.find(';') -
+                                                      levelString.size()),
+                                     ' ');
         }
         if (j % 2 == 1) {
-          spaces.push_back(levelString.size() - static_cast<int>(std::ceil(numOfSpaces/2.0))); //Not right forumula
+          spaces.push_back(levelString.size() -
+                           static_cast<int>(std::ceil(numOfSpaces / 2.0)));
         }
         levelString += std::format("{} ", element.toString());
-
       }
-      mvprintw(i, 0, "%s", levelString.c_str());
+      size_t termination = levelString.find("(-1;-1)");
+      while (termination < levelString.size()) {
+        levelString = levelString.replace(termination, 7, 7, ' ');
+        termination = levelString.find("(-1;-1)");
+      }
+      mvprintw(2 * i, 0, "%s", levelString.c_str());
+      if (i == 0) {
+        continue;
+      }
+      std::vector<size_t> semicolonPositions;
+
+      size_t pos = levelString.find(';', 0);
+      while(pos != std::string::npos)
+      {
+        semicolonPositions.push_back(pos);
+        pos = levelString.find(';',pos+1);
+      }
+      for (int j = 0; j < semicolonPositions.size(); j++) {
+        j % 2 == 0
+            ? mvprintw(2 * i - 1, static_cast<int>(semicolonPositions[j])+1, "/")
+            : mvprintw(2 * i - 1,
+                      static_cast<int>(semicolonPositions[j])-1,
+                      "\\");
+      }
     }
   };
   [[nodiscard]] virtual int getHeight() const {
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 int main() {
   initscr();
+  curs_set(0);
   Element elements[] ={{11, 11}, {22, 22}, {3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}, {8, 8}, {9, 9}, {10, 10}};
   Heap heap(elements, 10);
   heap.print();
