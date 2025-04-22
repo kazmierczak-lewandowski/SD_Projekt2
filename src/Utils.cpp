c1ba89d895f54c3e3be45b370dc1ffdcc3167941
Patryk Kazmierczak <github.scarcity013@passsmail.net>
2025-04-22 14:29:51 +0200

    Enhance Utils class: implement Gaussian distribution in gauss method and include algorithm header for random number generation

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,4 +1,6 @@
 #include "Utils.hpp"
+#include <algorithm>
+
 int Utils::rng(const int min, const int max) {
   std::random_device rd;
   std::mt19937 gen(rd());
@@ -6,5 +8,15 @@ int Utils::rng(const int min, const int max) {
   return dis(gen);
 }
 int Utils::gauss(const int min, const int max) {
-  return std::midpoint(min, max); // Placeholder for Gaussian distribution
+  const double mean = (min + max) / 2.0;
+  const double stddev = (max - min) / 6.0;
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::normal_distribution dist(mean, stddev);
+  double number = -1;
+  do {
+    number = dist(gen);
+  } while (number < min || number > max);
+  return static_cast<int>(number);
 }
