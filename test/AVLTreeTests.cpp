0db7148a29c273854c629e238b844f9c0d223019
kazik <github.scarcity013@passmail.net>
2025-05-07 12:17:09 +0200

    Fixed duplication in tests

--- test/AVLTreeTests.cpp
+++ test/AVLTreeTests.cpp
@@ -109,34 +109,6 @@ TEST(AVLTreeTests, LLRotationParentRight) {
   EXPECT_TRUE(isCorrect(avl.getRoot()));
   EXPECT_TRUE(isBalanced(avl.getRoot()));
 }
-// TEST(AVLTreeTests, getLevels) {
-//   AVLTree avl;
-//   avl.insert(Element{1, 10});
-//   avl.insert(Element{1, 8});
-//   avl.insert(Element{1, 11});
-//   avl.insert(Element{1, 5});
-//   avl.insert(Element{1, 9});
-//   avl.insert(Element{1, 12});
-//   avl.insert(Element{1, 6});
-//
-//   const auto levels = avl.getLevels();
-//
-//   const std::vector<std::vector<Element>> expectedLevels = {
-//     {{1,10}},
-//     {{1,8}, {1,11}},
-//     {{1,5}, {1,9}, {-1,-1}, {1,12}},
-//     {{-1,-1}, {1,6}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}}
-//   };
-//
-//   ASSERT_EQ(levels.size(), expectedLevels.size());
-//   for (size_t i = 0; i < levels.size(); ++i) {
-//     ASSERT_EQ(levels[i].size(), expectedLevels[i].size());
-//     for (size_t j = 0; j < levels[i].size(); ++j) {
-//       EXPECT_EQ(levels[i][j], expectedLevels[i][j]);
-//     }
-//   }
-// }
-
 TEST(AVLTreeTests, extractMax) {
   AVLTree avl;
   avl.insert(Element{1, 3});
@@ -182,7 +154,6 @@ TEST(AVLTreeTests, modifyKey) {
   avl.insert(Element{1, 9});
   avl.insert(Element{1, 7});
   avl.modifyKey(Element(1,10),13);
-  //avl.modifyKey(Element(1,10),13);
   EXPECT_TRUE(isCorrect(avl.getRoot()));
   EXPECT_TRUE(isBalanced(avl.getRoot()));
 }
@@ -193,20 +164,6 @@ TEST(AVLTreeTests, randomTree) {
     const int value = Utils::rng(0, 1'000'000);
     avl.insert(Element{key, value});
   }
-  ASSERT_EQ(avl.getSize(), 100'000);
-  ASSERT_TRUE(isCorrect(avl.getRoot()));
-  ASSERT_TRUE(isBalanced(avl.getRoot()));
-  const auto element = avl.getRandomElement();
-  const auto foundElement = avl.findElement(element);
-  ASSERT_TRUE(foundElement != nullptr);
-  EXPECT_NE(foundElement->element, Element(-1, -1));
-  EXPECT_TRUE(isCorrect(avl.getRoot()));
-  EXPECT_TRUE(isBalanced(avl.getRoot()));
-}
-TEST(AVLTreeTests, treeFromFile) {
-  AVLTree avl;
-  AVLTree::fillFromFile(avl, "/home/piotr/CLionProjects/SD_Projekt2/numbers.txt",
-                   100'000);
   ASSERT_EQ(avl.getSize(), 100'000);
   ASSERT_TRUE(isCorrect(avl.getRoot()));
   ASSERT_TRUE(isBalanced(avl.getRoot()));
